cdbba2e54c947de0598a2683ce92abf194ba1ac6
m3m0r7 <memory.style.serv@gmail.com>
2025-12-02 23:45:42 +0900

    wip

--- src/Instruction/Traits/InstructionBaseTrait.php
+++ src/Instruction/Traits/InstructionBaseTrait.php
@@ -24,25 +24,9 @@ trait InstructionBaseTrait
     use FlagsTrait;
     use IoPortTrait;
     use TaskSwitchTrait;
+    use SignTrait;
 
     public function __construct(protected InstructionListInterface $instructionList)
     {
-    }
-
-    /**
-     * Sign-extend a value from the given bit size to PHP int (64-bit).
-     */
-    protected function signExtend(int $value, int $bits): int
-    {
-        if ($bits >= 32) {
-            $value &= 0xFFFFFFFF;
-            return ($value & 0x80000000) ? $value - 0x100000000 : $value;
-        }
-
-        $mask = 1 << ($bits - 1);
-        $fullMask = (1 << $bits) - 1;
-        $value &= $fullMask;
-
-        return ($value & $mask) ? $value - (1 << $bits) : $value;
     }
 }
---
+++ src/Instruction/Traits/SignTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPMachineEmulator\Instruction\Traits;
+
+/**
+ * Trait for sign extension operations.
+ */
+trait SignTrait
+{
+    /**
+     * Sign-extend a value from the given bit size to PHP int (64-bit).
+     */
+    protected function signExtend(int $value, int $bits): int
+    {
+        if ($bits >= 32) {
+            $value &= 0xFFFFFFFF;
+            return ($value & 0x80000000) ? $value - 0x100000000 : $value;
+        }
+
+        $mask = 1 << ($bits - 1);
+        $fullMask = (1 << $bits) - 1;
+        $value &= $fullMask;
+
+        return ($value & $mask) ? $value - (1 << $bits) : $value;
+    }
+}
